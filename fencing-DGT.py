7f90f9301f4a609d8b8fa6d85a532a0bbba9e05b
B0ss-1 <168596551+B0ss-1@users.noreply.github.com>
2024-05-24 15:41:04 +1200

    Update fencing-DGT.py
    deaththreat is now is following coding conventions, and Perimeter (line 8) is now spelled correctly.

--- fencing-DGT.py
+++ fencing-DGT.py
@@ -7,21 +7,21 @@ print("Instuctions:")
 print()
 print("Enter the price of the fencing per meter.")
 print("After that enter the width and length.")
-print("Then this program will cauclate the peremeter.")
+print("Then this program will cauclate the perimeter.")
 print("and cost of fencing of the land you wanted to")
 print("find the price of fencing off.")
 print()
 def number_check(question):
-    Deaththreat="Put in a number bigger than 0. Or else.\n"
+    deaththreat="Put in a number bigger than 0. Or else.\n"
     while True:
         try:
             number=float(input(question))
             if number>0:
                 return number
             else:
-                print(Deaththreat)
+                print(deaththreat)
         except ValueError:
-            print(Deaththreat)
+            print(deaththreat)
 keepgoing=""
 while keepgoing=="":
     price=number_check("Price per meter of fence: $")
